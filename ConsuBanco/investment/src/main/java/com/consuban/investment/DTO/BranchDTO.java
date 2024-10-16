3a1a06e7c72e7f0de7e67ecab805d77678237b45
Ana Gabriela Campos <infoautomaticagc@gmail.com>
2024-10-15 21:59:54 -0600

    Branch

---
+++ ConsuBanco/investment/src/main/java/com/consuban/investment/DTO/BranchDTO.java
@@ -0,0 +1,33 @@
+package com.consuban.investment.DTO;
+
+public class BranchDTO {
+
+    private Long id;
+    private String branchName;
+    private String address;
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
